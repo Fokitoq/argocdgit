ed197b5203b7da34aa13258171649710c4623a15
Bohdan <happy4@ukr.net>
2024-01-28 21:50:48 +0200

    pushed updated configs

---
+++ Jenkins-CICD/variables.tf
@@ -0,0 +1,33 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     = "vprofile-prod-key"
+}
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     = "t2.medium"
+}
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-0c7217cdde317cfec"
+}
+variable "bucket_name" {
+  description = "The name of the S3 bucket to create"
+  type        = string
+  default     = "purplehaze777"
+}
+
+variable "dynamodb_table" {
+  description = "The name of the dynamodb table"
+  type        = string
+  default     = "purplehaze777-dynamodb-table"
+}
+
+variable "acl" {
+  description = "The ACL (Access Control List) for the S3 bucket"
+  type        = string
+  default     = "private"
+}
